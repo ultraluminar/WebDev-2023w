321a7ab4110c98902b5a64ee5d7e80691f521239
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-01-03 11:01:48 +0100

    Fix nav menu styling and prevent default anchor tag behavior

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleNav(event) {
-    event.prefentDefault(); // prevent default ancor tag behavior
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
     nav.style.display = nav.style.display === "flex" ? "none" : "flex";
     icon.classList.toggle('open');
+    event.prefentDefault(); // prevent default ancor tag behavior
 }
