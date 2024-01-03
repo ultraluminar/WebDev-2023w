498d3d91018e94fa9c534425c67990f9e3678acf
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-01-03 10:51:13 +0100

    Fix toggleNav event handler and update navigation styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function toggleNav() {
+function toggleNav(event) {
+    event.prefentDefault(); // prevent default ancor tag behavior
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
     nav.style.display = nav.style.display === "flex" ? "none" : "flex";
