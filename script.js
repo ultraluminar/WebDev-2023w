5edc99ebf3efbae4cd29af32f879f9b805871aa8
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-01-03 12:29:17 +0100

    Update header and navigation styling

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toggleNav(event) {
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+    let main = document.getElementById("main");
+    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-12.75em)" : "translateY(0%)";
+    main.style.transform = main.style.transform === "translateY(0%)" ? "translateY(-12.75em)" : "translateY(0%)";
     icon.classList.toggle('open');
     event.prefentDefault(); // prevent default ancor tag behavior
 }
