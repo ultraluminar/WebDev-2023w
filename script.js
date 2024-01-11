d35ae7092ee7a16faa56e4930fe14ffdaf025030
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-01-11 12:51:53 +0100

    Update HTML, JavaScript, and CSS files

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function toggleNav(event) {
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
     let main = document.getElementById("main");
-    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-12.75em)" : "translateY(0%)";
-    main.style.transform = main.style.transform === "translateY(0%)" ? "translateY(-12.75em)" : "translateY(0%)";
+    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-13em)" : "translateY(0%)";
+    main.style.transform = main.style.transform === "translateY(0%)" ? "translateY(-13em)" : "translateY(0%)";
     icon.classList.toggle('open');
     event.prefentDefault(); // prevent default ancor tag behavior
 }
