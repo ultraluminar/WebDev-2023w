f87346564a787cc47ccaa9de61b6bb988468e474
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-01-12 13:56:50 +0100

    Update logo and navigation styling

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function toggleNav(event) {
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
-    let main = document.getElementById("main");
     nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-13em)" : "translateY(0%)";
-    main.style.transform = main.style.transform === "translateY(0%)" ? "translateY(-13em)" : "translateY(0%)";
     icon.classList.toggle('open');
     event.prefentDefault(); // prevent default ancor tag behavior
 }
