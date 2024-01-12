be79963f300216d20321615e209dd40910edfabe
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-01-12 17:17:27 +0100

    Update navigation links styling and transform animation

--- quiz/script.js
+++ quiz/script.js
@@ -1,20 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let currentPage = window.location.pathname;
-    console.log(currentPage);
-    let navLinks = document.querySelectorAll("#nav a");
-    navLinks.forEach(function(link) {
-        console.log(link.getAttribute("href"));
-        if (link.getAttribute("href").substring(2) === currentPage) {
-            link.style.backgroundColor = "var(--secondary-color)";
-            link.style.color = "var(--text-color)";
-        }
-    });
-});
-
 function toggleNav(event) {
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
-    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-16em)" : "translateY(0%)";
+    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-17.5em)" : "translateY(0%)";
+    nav.style.boxShadow = nav.style.boxShadow === "0px 0px 50px -20px #b6b6b6;" ? "none" : "0px 0px 50px -20px #b6b6b6;";
     icon.classList.toggle('open');
     event.preventDefault(); // prevent default ancor tag behavior
 }
--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let currentPage = window.location.pathname.substring(1);
-    console.log(currentPage);
-    let navLinks = document.querySelectorAll("#nav a");
-    navLinks.forEach(function(link) {
-        console.log(link.getAttribute("href"));
-        if (link.getAttribute("href") === currentPage) {
-            link.style.backgroundColor = "var(--secondary-color)";
-            link.style.color = "var(--text-color)";
-        }
-    });
-});
-
 function toggleNav(event) {
     let nav = document.getElementById("nav");
     let icon = document.getElementById("burger-menu");
-    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-16em)" : "translateY(0%)";
+    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-17.5em)" : "translateY(0%)";
     icon.classList.toggle('open');
     event.preventDefault(); // prevent default ancor tag behavior
 }
