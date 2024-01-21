73d6e1c1221eabe2a45cfa610837d2f4cae81314
KlasL4243 <lukasklassen4243@gmail.com>
2024-01-21 22:21:09 +0100

    removed event

--- nav.js
+++ nav.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", async function () {
     document.getElementById("burger-menu").addEventListener("click", toggleNav);
 
-    function toggleNav(event) {
+    function toggleNav() {
         let nav = document.getElementById("nav");
         let icon = document.getElementById("burger-menu");
         nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-17.5em)" : "translateY(0%)";
         icon.classList.toggle('open');
-        event.preventDefault(); // prevent default ancor tag behavior
     }
 
 });
