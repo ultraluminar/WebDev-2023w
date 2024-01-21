723eef5dcfbf92a205885eab90be648334cff54d
KlasL4243 <lukasklassen4243@gmail.com>
2024-01-21 23:49:08 +0100

    delete files

--- lexikon/script.js
+++
@@ -1,7 +0,0 @@
-function toggleNav(event) {
-    let nav = document.getElementById("nav");
-    let icon = document.getElementById("burger-menu");
-    nav.style.transform = nav.style.transform === "translateY(0%)" ? "translateY(-17.5em)" : "translateY(0%)";
-    icon.classList.toggle('open');
-    event.preventDefault(); // prevent default ancor tag behavior
-}
